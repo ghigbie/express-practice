dcc1c2c0931ed77c382705d5520891ff72ed6f09
ghigbie <gehigbie@gmail.com>
2019-10-04 21:16:13 -0700

    Edit routes

--- first-practice/app.js
+++ first-practice/app.js
@@ -9,13 +9,16 @@ app.use('/', (req, res, next) => {
 
 app.use('/add-product', (req, res, next) => {
     console.log('In add product route');
+    console.log(req.body);
     res.send('<form action="/product" method="POST"><input type="text" /></form>');
 });
-app.use((req, res, next) => {
+
+app.use('/', (req, res, next) => {
     console.log('In another middleware');
-    res.send('<h1>Meow</h1>');
-    console.log('Meow run');
+    res.send('<h1>Meow Home!!</h1>');
+    console.log('Meow home run');
 });
+
 const server = http.createServer(app);
 
 server.listen(3000);
