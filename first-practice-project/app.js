07e29e98dd7ad9e79361a1f8157360e1908684f2
ghigbie <gehigbie@gmail.com>
2019-10-06 15:20:19 -0700

    Add html

---
+++ first-practice-project/app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+const rootRoute = require('./routes/root');
+const userRoutes = require('./routes/user');
+const errorRoutes = require('./routes/error');
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(rootRoute);
+app.use(userRoutes);
+app.use(errorRoutes);
+
+app.listen(3000);
