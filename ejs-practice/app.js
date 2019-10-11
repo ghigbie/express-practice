f5d66bc2475f948cfecdd5d525b0668a65d533ee
ghigbie <gehigbie@gmail.com>
2019-10-10 20:12:13 -0700

    Build out app.js

---
+++ ejs-practice/app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser')
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', 'views')
+
+const homeRoutes = require('./routes/home');
+const usersRoutes = require('./routes/users');
+const errorRoutes = require('./routes/error');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(homeRoutes);
+app.use(usersRoutes);
+app.use(errorRoutes);
+
+app.listen(3000);
