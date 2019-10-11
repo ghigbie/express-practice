0c77b314ec80e45f8b67a5a67c1562143f64418d
ghigbie <gehigbie@gmail.com>
2019-10-10 20:51:56 -0700

    Change order

--- ejs-practice/app.js
+++ ejs-practice/app.js
@@ -13,8 +13,8 @@ const errorRoutes = require('./routes/error');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(usersRoutes);
 app.use(homeRoutes);
+app.use(usersRoutes);
 app.use(errorRoutes);
 
 app.listen(3000);
