b51ba52f19777756987807d7018d7afe4800a35e
ghigbie <gehigbie@gmail.com>
2019-10-12 21:06:15 -0700

    Stuff is working

--- shop-mvc/app.js
+++ shop-mvc/app.js
@@ -1,6 +1,7 @@
+const path = require('path');
+
 const express = require('express');
 const bodyParser = require('body-parser');
-const path = require('path');
 
 const app = express();
 
@@ -9,11 +10,18 @@ app.set('views', 'views');
 
 const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const errorRoutes = require('./routes/error');
 
-app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin', adminData.routs);
+app.use('/admin', adminData.routes);
 app.use(shopRoutes);
+app.use(errorRoutes);
+
+// app.use((req, res, next) => {
+//     res.status(404).render('404', { pageTitle: 'Page Not Found' });
+// });
 
 app.listen(3000);
+
--- shop-mvc/routes/error.js
+++ shop-mvc/routes/error.js
@@ -4,11 +4,16 @@ const path = require('path');
 const rootDir = require('../util/path');
 const router = express.Router();
 
-router.get('/404.ejs', (req, res, next) => {
+router.get('/*', (req, res, next) => {
     const pageTitle = 'Page Not Found'
     const message = 'What are you doing with your life?';
-    res.render('/*', {
-        pageTitle,
-        message
-    })
+    res
+        .status(404)
+        .render('404', {
+            pageTitle,
+            message,
+            path: '/*'
+        });
 });
+
+module.exports = router;
