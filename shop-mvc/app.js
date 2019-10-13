7e6b2b3043f83b971020962a604670c4b44b6d1d
ghigbie <gehigbie@gmail.com>
2019-10-12 20:24:53 -0700

    Add new routes

---
+++ shop-mvc/app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+const adminData = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({ extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/admin', adminData.routs);
+app.use(shopRoutes);
+
+app.listen(3000);
