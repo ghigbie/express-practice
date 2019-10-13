4cbc940d2dc9ed83c319ad61aa8390aa51fe02a8
ghigbie <gehigbie@gmail.com>
2019-10-12 19:25:45 -0700

    Add new form

--- ejs-practice/routes/users.js
+++ ejs-practice/routes/users.js
@@ -4,17 +4,25 @@ const path = require('path');
 const rootDir = require('./../utils/path');
 
 const router = express.Router();
-
+const users = [
+    { name: 'Bubba' },
+    { name: 'Moo' },
+    { name: 'Ouch' },
+];
+const userName = '';
 router.get('/users', (req, res, next) => {
-    const users = [
-        {name: 'Bubba'},
-        {name: 'Moo'},
-        {name: 'Ouch'},
-    ];
-
     res.render('users', {
             pageTitle: 'Users',
-            users
+            users,
+            userName,
+        });
+});
+
+router.post('/add-user', (req, res, next) => {
+    res.redirect('/users', {
+            pageTitle: 'Users',
+            users,
+            userName,
         });
 });
 
