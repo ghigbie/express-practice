cef2ef39d77c852d5dc3b04765e5e160efac8f8c
ghigbie <gehigbie@gmail.com>
2019-10-14 20:41:22 -0700

    Add node files

--- shop-mvc/models/product.js
+++ shop-mvc/models/product.js
@@ -12,10 +12,29 @@ module.exports = class Product {
             'data',
             'products.json'
             );
+        fs.readFile(p, (err, fileContent) => {
+            let products = [];
+            if(!err){
+                prodcuts = JSON.parse(fileContent);
+            }
+            products.push(this);
+            fs.writeFile(p, JSON.stringify(products), (err) => {
+                console.log(err);
+            });
+        });
     }
 
-    static fetchAll(){
-        return products;
+    static fetchAll(cb){
+        const p = path.join(path.dirname(process.mainModule.filename),
+            'data',
+            'products.json'
+        );
+        fs.readFile(p, (err, fileContent) => {
+            if(err){
+                cb([]);
+            }
+            cb(JSON.parse(fileContent));
+        });
     }
     
 }
