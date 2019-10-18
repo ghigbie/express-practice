2f349c402ca15c632acb75a246cc148c356fa351
ghigbie <gehigbie@gmail.com>
2019-10-17 22:34:49 -0700

    Add new server file

--- node_aside/playground/index.js
+++ node_aside/playground/index.js
@@ -1,5 +1,14 @@
 const fs = require('fs');
 
-const textIn = fs.readFileSync('./txt/moo.txt', 'utf-8');
-const textOut = `Here is the file: ${textIn} on ${Date.now}`;
-fs.writeFile('./txt/output.txt', textOut);
+const textIn = fs.readFile('./txt/moo.txt', 'utf-8', (err, data)=> {
+    const textOut = data;
+    fs.writeFileSync('./txt/output.txt', textOut);
+});
+const textOut = `Here is the file: ${textIn} on ${Date.now()}`;
+fs.writeFileSync('./txt/output.txt', textOut);
+
+fs.readFile('./txt/mootwo.txt', 'utf-8', (err, data) => {
+   fs.writeFile('./txt/outputtwo.txt', data, err => {
+
+   });
+});
